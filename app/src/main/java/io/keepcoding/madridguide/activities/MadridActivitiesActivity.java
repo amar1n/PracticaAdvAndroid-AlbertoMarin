14b09e417186af92240a9e19275f22fdec409c84
Alberto Marín García <alberto.marin@gmail.com>
2017-01-13 02:39:04 +0100

    Se añaden los fragments de GoogleMaps

--- app/src/main/java/io/keepcoding/madridguide/activities/MadridActivitiesActivity.java
+++ app/src/main/java/io/keepcoding/madridguide/activities/MadridActivitiesActivity.java
@@ -3,6 +3,14 @@ package io.keepcoding.madridguide.activities;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 
 import io.keepcoding.madridguide.R;
 import io.keepcoding.madridguide.fragments.MadridActivitiesFragment;
@@ -12,9 +20,10 @@ import io.keepcoding.madridguide.model.MadridActivity;
 import io.keepcoding.madridguide.navigator.Navigator;
 import io.keepcoding.madridguide.util.OnElementClick;
 
-public class MadridActivitiesActivity extends AppCompatActivity {
+public class MadridActivitiesActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private MadridActivitiesFragment madridActivitiesFragment;
+    private SupportMapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +31,7 @@ public class MadridActivitiesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_madridactivities);
 
         madridActivitiesFragment = (MadridActivitiesFragment) getFragmentManager().findFragmentById(R.id.activity_madridactivities_fragment_madridactivities);
+        initilizeMap();
 
         GetAllMadridActivitiesFromLocalCacheInteractor interactor = new GetAllMadridActivitiesFromLocalCacheInteractor();
         interactor.execute(this, new GetAllMadridActivitiesFromLocalCacheInteractor.OnGetAllMadridActivitiesFromLocalCacheInteractorCompletion() {
@@ -38,4 +48,28 @@ public class MadridActivitiesActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void initilizeMap() {
+        if (mapFragment == null) {
+            mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+            // check if map is created successfully or not
+            if (mapFragment == null) {
+                Toast.makeText(getApplicationContext(), "Sorry! unable to create maps", Toast.LENGTH_SHORT).show();
+            } else {
+                mapFragment.getMapAsync(this);
+            }
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(
+                new LatLng(40.43807216375375, -3.6795366500000455)).zoom(11).build();
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+//        // Add a marker in Sydney, Australia, and move the camera.
+//        LatLng sydney = new LatLng(-34, 151);
+//        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
 }
--- app/src/main/java/io/keepcoding/madridguide/activities/ShopsActivity.java
+++ app/src/main/java/io/keepcoding/madridguide/activities/ShopsActivity.java
@@ -3,6 +3,14 @@ package io.keepcoding.madridguide.activities;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 
 import io.keepcoding.madridguide.R;
 import io.keepcoding.madridguide.fragments.ShopsFragment;
@@ -12,9 +20,10 @@ import io.keepcoding.madridguide.model.Shops;
 import io.keepcoding.madridguide.navigator.Navigator;
 import io.keepcoding.madridguide.util.OnElementClick;
 
-public class ShopsActivity extends AppCompatActivity {
+public class ShopsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private ShopsFragment shopsFragment;
+    private SupportMapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +31,7 @@ public class ShopsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_shops);
 
         shopsFragment = (ShopsFragment) getFragmentManager().findFragmentById(R.id.activity_shops_fragment_shops);
+        initilizeMap();
 
         GetAllShopsFromLocalCacheInteractor interactor = new GetAllShopsFromLocalCacheInteractor();
         interactor.execute(this, new GetAllShopsFromLocalCacheInteractor.OnGetAllShopsFromLocalCacheInteractorCompletion() {
@@ -37,5 +47,28 @@ public class ShopsActivity extends AppCompatActivity {
                 shopsFragment.setShops(shops);
             }
         });
+    }
+
+    private void initilizeMap() {
+        if (mapFragment == null) {
+            mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+            // check if map is created successfully or not
+            if (mapFragment == null) {
+                Toast.makeText(getApplicationContext(), "Sorry! unable to create maps", Toast.LENGTH_SHORT).show();
+            } else {
+                mapFragment.getMapAsync(this);
+            }
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(
+                new LatLng(40.43807216375375, -3.6795366500000455)).zoom(11).build();
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+//        // Add a marker in Sydney, Australia, and move the camera.
+//        LatLng sydney = new LatLng(-34, 151);
+//        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
